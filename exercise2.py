c0241e762836239fd890e2dbac8bd31a1c2199a8
Özgür Doğa Bulut <zgrdgblt@gmail.com>
2022-05-23 20:02:18 +0300

    Solution for Exercise 2

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+first_name = input("Please Enter Your First Name: ")
+last_name = input("Please Enter Your Last Name: ")
+
+print("Hello", first_name.capitalize(), last_name.capitalize())
+
+#Exercise for print, for doing more complex ı also add capitalize function
