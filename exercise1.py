f761e1926f4ae851566dba383ea1d40d62bdc7b3
Özgür Doğa Bulut <zgrdgblt@gmail.com>
2022-05-23 19:54:15 +0300

    Solution of Exercise 1

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+#Create a program that displays your name and complete mailing address
+
+mailing_adress = '''
+Adress
+
+
+'''
+#This exercise is created for print function because of that I use only print function (no input)
+
+print(mailing_adress)
