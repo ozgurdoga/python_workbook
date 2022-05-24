e56a54108963067a615a636c05dc7fd54fd45a32
Özgür Doğa Bulut <zgrdgblt@gmail.com>
2022-05-24 18:12:23 +0300

    Solution for Exercise3

---
+++ exercise3.py
@@ -0,0 +1,11 @@
+from turtle import width
+
+
+width = float(input("Please enter your room of the width in meters: "))
+length = float(input("Please enter your room of the length in meters"))
+area_room = width * length
+
+print("Your Room Width =", width, "Meters")
+print("Your Room Length =",length, "Meters")
+print("The Area of Room =", area_room)
+
