df5493498944dbe096f6a5a0c30487a4ccd6660c
Özgür Doğa Bulut <zgrdgblt@gmail.com>
2022-05-25 00:14:55 +0300

    Solution for Exercise4

---
+++ exercise4.py
@@ -0,0 +1,6 @@
+width = float(input("Enter width of Farmer's field: "))
+length = float(input("Enter length of Farmer's field: "))
+square_feet = width * length
+acres = float(square_feet / 43560)
+print("Area of the field in acres = ", acres)
+
